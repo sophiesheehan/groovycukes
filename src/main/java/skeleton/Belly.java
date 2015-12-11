82fc8c9fb6d75b3b58f93224f37a8611753c5d96
sophie.darling <sophie.darling@ymail.com>
2015-12-11 10:24:21 -0500

    Added code for pending steps

--- src/main/java/skeleton/Belly.java
+++ src/main/java/skeleton/Belly.java
@@ -1,7 +1,22 @@
 package skeleton;
 
 public class Belly {
+    private static final int BELLY_MAX_CUKES = 41;
+    private static final int BELLY_MAX_TIME = 1;
+    private static final String BELLY_GROWL = "GROWL";
+    private static final String BELLY_SILENT = "silent";
+    private int cukes;
+  
     public void eat(int cukes) {
-
+        this.cukes = cukes;
     }
+  
+  public String getSound ( int waitingTime ) {
+      if (this.cukes > this.BELLY_MAX_CUKES && waitingTime >= this.BELLY_MAX_TIME) {
+        return this.BELLY_GROWL;
+      }
+      else {
+        return this.BELLY_SILENT;
+      }
+  }
 }
